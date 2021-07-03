d0bcd59982d517bf4d5e35f550a2453801fd1c88
Sergey Grankin <sgrankin@gmail.com>
2021-07-03 19:59:23 -0400

    git-review
    Change-ID: 1upb1mHd7B6fZzXw8sL50tnMMfg

---
+++ cmd/git-review/main.go
@@ -0,0 +1,163 @@
+//git-review will create PRs for review
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object/commitgraph"
+
+	"github.com/sgrankin/git-stacked/internal/change"
+	"github.com/sgrankin/git-stacked/internal/git"
+	gh2 "github.com/sgrankin/git-stacked/internal/github"
+)
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	repo, err := git.Open()
+	if err != nil {
+		log.Fatalf("git open: %v", err)
+	}
+	remote, err := repo.GetCurrentRemoteURL()
+	if err != nil {
+		log.Fatalf("getDefaultBranch: %s", err)
+	}
+
+	gh, err := gh2.Discover(ctx, remote)
+	if err != nil {
+		log.Fatalf("discover: %s", err)
+	}
+	base := gh.DefaultBranch()
+
+	baseHash, err := repo.ResolveRevision(base)
+	if err != nil {
+		log.Fatal(err)
+	}
+	baseCommits, err := getAllCommits(repo, *baseHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	headHash, err := repo.ResolveRevision(string(plumbing.HEAD))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commits, err := getNewCommits(repo, *headHash, baseCommits)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(commits)
+
+	changes, err := ensureChangeID(gh, repo, commits)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := doPush(gh, repo, changes); err != nil {
+		log.Fatal(err)
+	}
+	if err := syncPRs(gh, repo, changes); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getAllCommits(repo *git.Repo, start plumbing.Hash) (map[plumbing.Hash]bool, error) {
+	seen := map[plumbing.Hash]bool{}
+	err := repo.WalkCommits(start, func(cn commitgraph.CommitNode) error {
+		seen[cn.ID()] = true
+		return nil
+	})
+	return seen, err
+}
+
+func getNewCommits(repo *git.Repo, start plumbing.Hash, end map[plumbing.Hash]bool) ([]plumbing.Hash, error) {
+	var result []plumbing.Hash
+	if err := repo.WalkCommits(start, func(cn commitgraph.CommitNode) error {
+		if end[cn.ID()] {
+			return git.SkipCommit
+		}
+		// TODO: error on merge commits?
+		result = append(result, cn.ID())
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	// Reverse the results so that the commits are in commit-time order.
+	for i := len(result)/2 - 1; i >= 0; i-- {
+		opp := len(result) - 1 - i
+		result[i], result[opp] = result[opp], result[i]
+	}
+	return result, nil
+}
+
+type Change struct {
+	*change.Change
+	Head string
+	Base string
+}
+
+func ensureChangeID(gh *gh2.Client, repo *git.Repo, commits []plumbing.Hash) ([]*Change, error) {
+	if len(commits) == 0 {
+		return nil, nil
+	}
+	lastHead := gh.DefaultBranch()
+
+	var changes []*Change
+	var lastHash *plumbing.Hash
+	for _, h := range commits {
+		change, err := change.Ensure(repo, h, lastHash)
+		if err != nil {
+			return nil, err
+		}
+		lastHash = &change.Hash
+		c := &Change{
+			Change: change,
+			Head:   fmt.Sprintf("%s/%s", gh.Username(), change.ID),
+			Base:   lastHead,
+		}
+		changes = append(changes, c)
+		lastHead = c.Head
+
+	}
+	return changes, repo.UpdateHead(*lastHash)
+}
+
+func syncPRs(gh *gh2.Client, repo *git.Repo, changes []*Change) error {
+	ctx := context.Background()
+	for _, c := range changes {
+		pr, err := gh.GetPull(ctx, c.Head)
+		if err != nil {
+			return err
+		}
+		if pr == nil {
+			pr, err = gh.CreatePull(ctx, c.Head, c.Base, c.Title, c.Body)
+		} else {
+			pr, err = gh.UpdatePull(ctx, *pr.Number, c.Base, c.Title, c.Body)
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("Updated PR: %s", *pr.HTMLURL)
+	}
+	return nil
+}
+
+func doPush(gh *gh2.Client, repo *git.Repo, changes []*Change) error {
+	refSpecs := map[plumbing.Hash]string{}
+	for _, c := range changes {
+		refSpecs[c.Hash] = c.Head
+	}
+	return gh.Push(repo.Storer(), refSpecs)
+}
---
+++ internal/github/github.go
@@ -0,0 +1,145 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage"
+	"github.com/google/go-github/v36/github"
+	"golang.org/x/oauth2"
+)
+
+type Client struct {
+	token    string
+	username string
+
+	owner string
+	repo  string
+
+	defaultBranch string
+}
+
+var githubURL = regexp.MustCompile(`^(?:https://github.com/|git@github.com:)([^/]+)/(.+).git$`)
+
+func Discover(ctx context.Context, remoteURL string) (*Client, error) {
+	log.Println(remoteURL)
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return nil, fmt.Errorf("GITHUB_TOKEN env must be set to a valid GitHub PAT")
+	}
+	gh := ghClient(ctx, token)
+	user, _, err := gh.Users.Get(ctx, "")
+	if err != nil {
+		return nil, err
+	}
+
+	matches := githubURL.FindStringSubmatch(remoteURL)
+	if matches == nil {
+		return nil, fmt.Errorf("remote URL %q did not match expected pattern %s", remoteURL, githubURL)
+	}
+	owner := matches[1]
+	repo := matches[2]
+
+	r, _, err := gh.Repositories.Get(ctx, owner, repo)
+	if err != nil {
+		return nil, err
+	}
+	defaultBranch := r.DefaultBranch
+
+	return &Client{
+		token:         token,
+		username:      *user.Login,
+		owner:         owner,
+		repo:          repo,
+		defaultBranch: *defaultBranch,
+	}, nil
+}
+
+func (c *Client) Username() string {
+	return c.username
+}
+func (c *Client) DefaultBranch() string {
+	return c.defaultBranch
+}
+
+// `refs` is a map from hash to remote head name.
+func (c *Client) Push(storer storage.Storer, refs map[plumbing.Hash]string) error {
+	var refSpecs []config.RefSpec
+	for h, ref := range refs {
+		refSpecs = append(refSpecs, config.RefSpec(
+			fmt.Sprintf("%s:refs/heads/%s", h, ref)))
+	}
+
+	err := git.
+		NewRemote(storer, &config.RemoteConfig{
+			Name: "github",
+			URLs: []string{fmt.Sprintf("https://github.com/%s/%s.git", c.owner, c.repo)},
+		}).
+		Push(&git.PushOptions{
+			RemoteName: "github",
+			RefSpecs:   refSpecs,
+			Auth: &http.BasicAuth{
+				Username: c.username,
+				Password: c.token,
+			},
+			Progress: os.Stderr,
+			Force:    true,
+		})
+	if err == git.NoErrAlreadyUpToDate {
+		return nil
+	}
+	return err
+}
+
+func ghClient(ctx context.Context, token string) *github.Client {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	return github.NewClient(tc)
+}
+
+func (c *Client) GetPull(ctx context.Context, head string) (*github.PullRequest, error) {
+	ghClient := ghClient(ctx, c.token)
+	prs, _, err := ghClient.PullRequests.List(ctx, c.owner, c.repo, &github.PullRequestListOptions{
+		Head: fmt.Sprintf("%s:%s", c.username, head),
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(prs) > 1 {
+		log.Fatalf("Found multiple PRs for head: %s", head)
+	}
+	if len(prs) == 0 {
+		return nil, nil
+	}
+	return prs[0], nil
+}
+
+func (c *Client) CreatePull(ctx context.Context, head, base, title, body string) (*github.PullRequest, error) {
+	ghClient := ghClient(ctx, c.token)
+	pr, _, err := ghClient.PullRequests.Create(ctx,
+		c.owner, c.repo, &github.NewPullRequest{
+			Head:  &head,
+			Base:  &base,
+			Title: &title,
+			Body:  &body,
+		})
+	return pr, err
+}
+
+func (c *Client) UpdatePull(ctx context.Context, pull int, base, title, body string) (*github.PullRequest, error) {
+	ghClient := ghClient(ctx, c.token)
+	pr, _, err := ghClient.PullRequests.Edit(ctx,
+		c.owner, c.repo, pull, &github.PullRequest{
+			Base:  &github.PullRequestBranch{Ref: &base},
+			Title: &title,
+			Body:  &body,
+		})
+	return pr, err
+}
